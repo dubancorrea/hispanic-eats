014f16de0ef226ed97365cc4418c17c5f05c8b6e
Duban Correa <DUBAN.CORREAGARZON99@bcmail.cuny.edu>
2026-03-03 15:23:06 -0500

    Connected Board to App and added card styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
 
+import Board from './components/Board'; 
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Hispanic Eats 🌮</h1>
-      {/* We will put our Board component here later */}
+      <header>
+        <h1>Hispanic Eats 🌮</h1>
+        {/* 2. Add the subtitle requirement from the project overview */}
+        <h2>Authentic flavors from the heart of our community</h2>
+      </header>
+      
+      {/* 3. Render the Board here */}
+      <Board />
     </div>
   )
 }
